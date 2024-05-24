60b1ab3918635d0bb45f4a49b5babe855a62b017
feature/sc <tjdcjf0781@gmail.com>
2024-05-24 17:56:01 +0900

    8393번 문제해결

--- src/p8393.java
+++ src/p8393.java
@@ -2,8 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
+public class p8393 {
+    public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int input = Integer.parseInt(bf.readLine());
